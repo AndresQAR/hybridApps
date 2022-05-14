3e8fa9a4a282eae49d08fd0e29e7d7bcf5f5b8ab
andy <aquintero.ar@gmail.com>
2022-05-14 18:04:47 -0300

    Clase 15

---
+++ clase_15/controllers/students.api.controllers.js
@@ -0,0 +1,91 @@
+import * as studentsModel from '../services/students.service.js'
+
+function getByID (req, res) {
+  const id = parseInt(req.params.studentId)
+
+  studentsModel.findByID(id)
+    .then(function (student) {
+      if (student) {
+        res.status(200).json(student)
+      } else {
+        res.status(404).json({ message: `Student #${id} cannot be found` })
+      }
+    })
+}
+
+function getAll (req, res) {
+  studentsModel.find()
+    .then(function (student) {
+      res.status(200).json(student)
+    })
+}
+
+function create (req, res) {
+  const student = req.body
+  studentsModel.create(student)
+    .then(function (newStudent) {
+      res.status(201).json(newStudent)
+    })
+    .catch(function (err) {
+      res.status(500).json({ err })
+    })
+}
+
+function remove (req, res) {
+  const id = parseInt(req.params.studentId)
+
+  studentsModel.remove(id)
+    .then(function (student) {
+      if (student) {
+        res.status(200).json(student)
+      } else {
+        res.status(404).json({ message: `Student #${id} cannot be found` })
+      }
+    })
+    .catch(function (err) {
+      res.status(500).json({ err })
+    })
+}
+
+function replace (req, res) {
+  const id = parseInt(req.params.studentId)
+  const studentData = req.body
+
+  studentsModel.replace(id, studentData)
+    .then(function (student) {
+      if (student) {
+        res.status(200).json(student)
+      } else {
+        res.status(404).json({ message: `Student #${id} cannot be found` })
+      }
+    })
+    .catch(function (err) {
+      res.status(500).json({ err })
+    })
+}
+
+function update (req, res) {
+  const id = parseInt(req.params.studentId)
+  const studentData = req.body
+
+  studentsModel.update(id, studentData)
+    .then(function (student) {
+      if (student) {
+        res.status(200).json(student)
+      } else {
+        res.status(404).json({ message: `Student #${id} cannot be found` })
+      }
+    })
+    .catch(function (err) {
+      res.status(500).json({ err })
+    })
+}
+
+export default {
+  getByID,
+  getAll,
+  create,
+  remove,
+  replace,
+  update
+}
